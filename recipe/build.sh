b8acf07eecb7ef82fab086f4c3043a5ac8be2bd5
K Mart <krmartin@hrl.com>
2024-12-03 20:17:19 -0800

    Removing patch files, and update meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,10 @@ mkdir -p ${PREFIX}/lib
 mv lib/* ${PREFIX}/lib
 
 for i in ${PREFIX}/lib/ollama/libcudart.so ${PREFIX}/lib/ollama/libggml_cuda_v11.so ${PREFIX}/lib/ollama/libggml_cuda_v12.so; do
+  echo 'KEVIN KEVIN KEVIN'
+  echo 'ORIGIN: ' $ORIGIN
+  echo 'PREFIX: ' ${PREFIX}
+  ls -lrth ${PREFIX}/lib/ollama
+  ls -lR ${PREFIX}/bin
   patchelf --set-rpath '$ORIGIN' $i
 done
